7e860cc12d60dd9d2f4dd5ade789d420168f87d0
Ly0t <eliott.bouquerel@gmail.com>
2025-02-07 22:51:20 +0100

    wss

--- src/hooks/useWebSocket.ts
+++ src/hooks/useWebSocket.ts
@@ -34,7 +34,7 @@ const useWebSocket = (
 
   useEffect(() => {
     const newSocket = new WebSocket(
-      `ws://eliott.bouquerel.caen.mds-project.fr:${port}`
+      `wss://eliott.bouquerel.caen.mds-project.fr:${port}`
     );
     setSocket(newSocket);
     return () => newSocket.close();
