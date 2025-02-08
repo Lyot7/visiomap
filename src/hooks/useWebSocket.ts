159d28ade7418c6b26a4b2587eadb7009c754072
Ly0t <eliott.bouquerel@gmail.com>
2025-02-08 13:35:26 +0100

    ws

--- src/hooks/useWebSocket.ts
+++ src/hooks/useWebSocket.ts
@@ -35,7 +35,7 @@ const useWebSocket = (
   useEffect(() => {
     console.log("Attempting to open WebSocket connection...");
     const newSocket = new WebSocket(
-      `wss://eliott.bouquerel.caen.mds-project.fr:7864`
+      `ws://eliott.bouquerel.caen.mds-project.fr:7864`
     );
     setSocket(newSocket);
 
